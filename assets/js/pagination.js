245a07f53740b7bda7ac1bb8f681c2c185e8b537
Mehwish <mehwishgul715@gmail.com>
2023-04-18 11:04:29 +0500

    commit

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,19 +1,17 @@
 // ======================================================================================pagination.js
-$(document).ready(function() {
-    var itemsPerPage = 8;
-    var productCards = $('.product-card"');
-    console.log(productCards);
-    
+$(document).ready(function () {
+    var itemsPerPage = 2;
+    var productCards = $('.mall-product');
+
     // Initialize pagination.js
     $('#pagination-container').pagination({
-      dataSource: productCards,
-      pageSize: itemsPerPage,
-      callback: function(data, pagination) {
-        // Display the data for the current page
-        var startIndex = pagination.pageNumber * pagination.pageSize;
-        var endIndex = startIndex + pagination.pageSize;
-        $('#product-cards-container').html(data.slice(startIndex, endIndex));
-      }
+        dataSource: productCards,
+        pageSize: itemsPerPage,
+        callback: function (data, pagination) {
+            // Display the data for the current page
+            var startIndex = pagination.pageNumber * pagination.pageSize;
+            var endIndex = startIndex + pagination.pageSize;
+            $('#product-cards-container').html(data.slice(startIndex, endIndex));
+        }
     });
-  });
-  
+});
