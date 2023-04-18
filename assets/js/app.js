25e03cd27bda5b629124f2404902dcd639939275
Mehwish <mehwishgul715@gmail.com>
2023-04-18 17:06:57 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -112,3 +112,109 @@ $('.partners').owlCarousel({
         }
     }
 })
+// ==============================================================inc/dec buttons
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+let a = 1;
+plus.addEventListener("click", ()=>{
+  a++;
+  a = (a < 10) ? "0" + a : a;
+  num.innerText = a;
+});
+
+minus.addEventListener("click", ()=>{
+  if(a > 1){
+    a--;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+  }
+});
+
+
+// ==========================================================single products tabs
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    
+  }
+//   $(".defaultOpen").click(); 
+// ===============================================rating system
+$(document).ready(function () {
+    $("#input-21f").rating({
+        starCaptions: function (val) {
+            if (val < 3) {
+                return val;
+            } else {
+                return 'high';
+            }
+        },
+        starCaptionClasses: function (val) {
+            if (val < 3) {
+                return 'label label-danger';
+            } else {
+                return 'label label-success';
+            }
+        },
+        hoverOnClear: false
+    });
+    var $inp = $('#rating-input');
+
+    $inp.rating({
+        min: 0,
+        max: 5,
+        step: 1,
+        size: 'lg',
+        showClear: false
+    });
+
+    $('#btn-rating-input').on('click', function () {
+        $inp.rating('refresh', {
+            showClear: true,
+            disabled: !$inp.attr('disabled')
+        });
+    });
+
+
+    $('.btn-danger').on('click', function () {
+        $("#kartik").rating('destroy');
+    });
+
+    $('.btn-success').on('click', function () {
+        $("#kartik").rating('create');
+    });
+
+    $inp.on('rating.change', function () {
+        alert($('#rating-input').val());
+    });
+
+
+    $('.rb-rating').rating({
+        'showCaption': true,
+        'stars': '3',
+        'min': '0',
+        'max': '3',
+        'step': '1',
+        'size': 'xs',
+        'starCaptions': {0: 'status:nix', 1: 'status:wackelt', 2: 'status:geht', 3: 'status:laeuft'}
+    });
+    $("#input-21c").rating({
+        min: 0, max: 8, step: 0.5, size: "xl", stars: "8"
+    });
+});
--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,17 +1,48 @@
-// ======================================================================================pagination.js
-$(document).ready(function () {
-    var itemsPerPage = 2;
-    var productCards = $('.mall-product');
+// $(document).ready(function() {
+//     var products = $(".mall-product");
+//     var data = []
+//     products.each(function() {
+//         data.push({id: $(this).attr("id")})
+//     })
 
-    // Initialize pagination.js
-    $('#pagination-container').pagination({
-        dataSource: productCards,
-        pageSize: itemsPerPage,
-        callback: function (data, pagination) {
-            // Display the data for the current page
-            var startIndex = pagination.pageNumber * pagination.pageSize;
-            var endIndex = startIndex + pagination.pageSize;
-            $('#product-cards-container').html(data.slice(startIndex, endIndex));
-        }
-    });
-});
+//     var pageSize = 4
+    
+//     $("#pagination-container").pagination({
+//       dataSource: data,
+//       pageSize: pageSize,
+//       className: '',
+//       callback: function(data, pagination) {
+//         handlePagination(data, pagination.pageNumber);
+//       }
+//     });
+
+
+//     function handlePagination(data, pageNumber) {
+//         // Calculate the start and end indexes of the current page
+//         var startIndex = (pageNumber - 1) * pageSize;
+//         var endIndex = startIndex + pageSize;
+
+//         console.log(data, startIndex, endIndex);
+        
+//         // Slice the data array to get only the products for the current page
+//         // var products = data.slice(startIndex, endIndex);
+//         var products2 = data;
+    
+        
+//         // Hide all product cards
+//         $(".mall-product").hide();
+//         // $(".grid-mall").empty()
+
+//         // console.log(products);
+        
+//         // Show only the product cards for the current page
+//         $.each(products2, function(index, product) {
+//         //     console.log(product.id);
+//           $("#" + product.id).show();
+//         // console.log(products.filter("#" + product.id));
+//         //     $(".grid-mall").append(products.filter("#" + product.id))
+//         });
+//       }
+      
+//   });
+  
