2ce211f63c135a7c097650b25d1f27fb8af049bc
Mehwish <mehwishgul715@gmail.com>
2023-04-29 23:06:58 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -135,14 +135,7 @@ $('.related-products').owlCarousel({
         }
     }
 })
-// ========================================================carousel dragable
-$(".carousel").swipe({
-    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-        if (direction == 'left') $(this).carousel('next');
-        if (direction == 'right') $(this).carousel('prev');
-    },
-    allowPageScroll: "vertical" 
-});
+
 
 
 
