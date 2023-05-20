0ac7f8c4c2a6410492157d99d07849dc7d27e723
Mehwish <mehwishgul715@gmail.com>
2023-05-20 17:55:58 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,18 @@
 // ========================================================================AOS
 AOS.init();
 
-
-
 // ========================================================================navbar close on scroll and click
-$('.open-icon').on('click',function(){
-    $('.close-icon').css('display','block');
-    $(this).css('display','none');
-    $('.menu-links').css('left','0');
-    $('body').css('overflow-y','hidden');
+$('.open-icon').on('click', function () {
+  $('.close-icon').css('display', 'block');
+  $(this).css('display', 'none');
+  $('.menu-links').css('left', '0');
+  $('body').css('overflow-y', 'hidden');
 });
-$('.close-icon').on('click',function(){
-    $(this).css('display','none');
-    $('.open-icon').css('display','block');
-    $('.menu-links').css('left','-300px');
-    $('body').css('overflow-y','auto');
+$('.close-icon').on('click', function () {
+  $(this).css('display', 'none');
+  $('.open-icon').css('display', 'block');
+  $('.menu-links').css('left', '-300px');
+  $('body').css('overflow-y', 'auto');
 });
 
 // =========================================================Load More
@@ -108,7 +106,6 @@ $('.close-icon').on('click',function(){
 // </div>`);
 // });
 
-
 // $(function () {
 //     $(document).click(function (event) {
 //         var navbar = $(".collapse");
@@ -137,113 +134,110 @@ $('.close-icon').on('click',function(){
 //     prevScrollpos = currentScrollPos;
 //   }
 
-
 //   ================================================================================Flags dropdown select
 function format(item, state) {
-    if (!item.id) {
-        return item.text;
-    }
-    var countryUrl = "https://hatscripts.github.io/circle-flags/flags/";
-    var stateUrl = "https://oxguy3.github.io/flags/svg/us/";
-    var url = state ? stateUrl : countryUrl;
-    var img = $("<img>", {
-        class: "img-flag",
-        width: 26,
-        src: url + item.element.value.toLowerCase() + ".svg"
-    });
-    var span = $("<span>", {
-        text: " " + item.text
-    });
-    span.prepend(img);
-    return span;
+  if (!item.id) {
+    return item.text;
+  }
+  var countryUrl = 'https://hatscripts.github.io/circle-flags/flags/';
+  var stateUrl = 'https://oxguy3.github.io/flags/svg/us/';
+  var url = state ? stateUrl : countryUrl;
+  var img = $('<img>', {
+    class: 'img-flag',
+    width: 26,
+    src: url + item.element.value.toLowerCase() + '.svg',
+  });
+  var span = $('<span>', {
+    text: ' ' + item.text,
+  });
+  span.prepend(img);
+  return span;
 }
 
 $(document).ready(function () {
-    $("#countries").select2({
-        templateResult: function (item) {
-            return format(item, false);
-        },
-        templateSelection: function (item) {
-            return format(item, false);
-        }
-    });
-
+  $('#countries').select2({
+    templateResult: function (item) {
+      return format(item, false);
+    },
+    templateSelection: function (item) {
+      return format(item, false);
+    },
+  });
 });
-
-
 
 //   ===================================================================================================carousel
 $('.testimonials').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 2,
-            nav: false
-        },
-        1000: {
-            items: 3,
-            nav: false,
-        }
-    }
-})
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
 
 $('.partners').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 2,
-            nav: false
-        },
-        1000: {
-            items: 4,
-            nav: false,
-
-        }
-    }
-})
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 4,
+      nav: false,
+    },
+  },
+});
 
 $('.related-products').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 2,
-            nav: false
-        },
-        600: {
-            items: 3,
-            nav: false
-        },
-        1000: {
-            items: 4,
-            nav: false,
-        }
-    }
-})
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2,
+      nav: false,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 4,
+      nav: false,
+    },
+  },
+});
 
-
-
-
-
+if ($('#action-nav').length) {
+  window.addEventListener('load', function () {
+    var actionNav = document.getElementById('action-nav');
+    actionNav.classList.remove('v-hidden');
+  });
+}
